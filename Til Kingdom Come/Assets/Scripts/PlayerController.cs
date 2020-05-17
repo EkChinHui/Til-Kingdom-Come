5b068b4bd6fadcce0ede4b4dfc692be8019c95ca
EkChinHui <57344608+EkChinHui@users.noreply.github.com>
2020-05-17 22:19:36 +0800

    Add animation transition logic
    for the engine to recognize when to do a certain animation based on a certain input from the input manager.

--- Til Kingdom Come/Assets/Scripts/PlayerController.cs
+++ Til Kingdom Come/Assets/Scripts/PlayerController.cs
@@ -1,38 +1,78 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
-    //private Animator anim;
+    private Animator anim;
+
+    private enum State { idle, run, attack, block, roll, die}
+    private State state = State.idle;
+
+    [SerializeField] private int health = 1;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     public void Update()
     {
+        Move();
+    }
+
+    private void Move()
+    {
         float hDirection = Input.GetAxis("Horizontal");
         float vDirection = Input.GetAxis("Vertical");
+        float Attack = Input.GetAxis("Fire1");
 
         if (hDirection < 0)
         {
             rb.velocity = new Vector2(-5, rb.velocity.y);
             transform.localScale = new Vector2(-1, 1);
-        } else if (hDirection > 0)
+            state = vDirection < 0 ? State.roll : State.run;
+        }
+        else if (hDirection > 0)
         {
             rb.velocity = new Vector2(5, rb.velocity.y);
             transform.localScale = new Vector2(1, 1);
-        } else
+            state = vDirection < 0 ? State.roll : State.run;
+        }
+        else
         {
-
+            state = State.idle;
         }
 
+
+        if (Attack != 0)
+        {
+            state = State.attack;
+        }
+
+
+        // jump
+        /*
         if(vDirection > 0)
         {
             rb.velocity = new Vector2(rb.velocity.x, 10f);
+            state = State.jump;
+        }
+        */
+        anim.SetInteger("state", (int)state);
+    }
+
+
+    /*
+    public void OnCollisionEnter(Collision2D other)
+    {
+        if(other.gameObject.tag == "Enemy")
+        {
+            Enemy enemy = other.gameObject.GetComponent<Enemy>();
         }
     }
+    */
 }
