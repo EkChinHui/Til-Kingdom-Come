e9162cf765cd5ed7744aa1c1c27febd75c87845b
EkChinHui <57344608+EkChinHui@users.noreply.github.com>
2020-05-17 15:20:54 +0800

    Create script for basic movement

---
+++ Til Kingdom Come/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    //private Animator anim;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void Update()
+    {
+        float hDirection = Input.GetAxis("Horizontal");
+        float vDirection = Input.GetAxis("Vertical");
+
+        if (hDirection < 0)
+        {
+            rb.velocity = new Vector2(-5, rb.velocity.y);
+            transform.localScale = new Vector2(-1, 1);
+        } else if (hDirection > 0)
+        {
+            rb.velocity = new Vector2(5, rb.velocity.y);
+            transform.localScale = new Vector2(1, 1);
+        } else
+        {
+
+        }
+
+        if(vDirection > 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 10f);
+        }
+    }
+}
