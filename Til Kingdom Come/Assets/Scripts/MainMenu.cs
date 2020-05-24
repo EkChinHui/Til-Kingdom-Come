501c7caf7f8485d6fa6c337d401de3fed9770ec6
EkChinHui <57344608+EkChinHui@users.noreply.github.com>
2020-05-24 11:58:53 +0800

    Change script for loading new scene

--- Til Kingdom Come/Assets/Scripts/MainMenu.cs
+++ Til Kingdom Come/Assets/Scripts/MainMenu.cs
@@ -6,10 +6,15 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] public string mapToLoad;
-    
-    public void PlayGame()
+
+    public void LoadMap()
     {
         SceneManager.LoadScene(mapToLoad);
+    }
+
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     public void QuitGame()
