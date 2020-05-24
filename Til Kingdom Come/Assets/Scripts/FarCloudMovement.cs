774db9c4a57535bb6032289e3dc0b042faa7f488
wenhao <wenhaogoh1998@gmail.com>
2020-05-24 18:54:09 +0800

    Add animations for menu screen
    Removed 'The Great History' button as it is no longer in use.

---
+++ Til Kingdom Come/Assets/Scripts/FarCloudMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FarCloudMovement : MonoBehaviour
+{
+    RectTransform rectTransform;
+    float speed = -50f;
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+        //update the position
+        if (rectTransform.anchoredPosition.x <= -1920) {
+            rectTransform.anchoredPosition = new Vector2(1920, 0);
+        }
+
+        rectTransform.anchoredPosition += new Vector2(Time.deltaTime * speed, 0);
+    }
+}
---
+++ Til Kingdom Come/Assets/Scripts/NearCloudMovement1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NearCloudMovement1 : MonoBehaviour
+{
+    RectTransform rectTransform;
+    float speed = -100f;
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+        //update the position
+        if (rectTransform.anchoredPosition.x <= -1920) {
+            rectTransform.anchoredPosition = new Vector2(1920, 0);
+        }
+
+        rectTransform.anchoredPosition += new Vector2(Time.deltaTime * speed, 0);
+    }
+}
