e6ef920c4e73c5a1589577e0933013a0d8aa0623
wenhao <wenhaogoh1998@gmail.com>
2020-05-28 22:55:19 +0800

    Add cross fade transition from main menu to map selection screen

---
+++ Til Kingdom Come/Assets/Scripts/LevelLoaderController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoaderController : MonoBehaviour
+{
+    public Animator transition;
+    public float transitionTime = 1f;
+    public string sceneName = "Map Selection";
+    public void LoadLevel(string sceneName) {
+        StartCoroutine(Helper(sceneName));
+    }
+
+    IEnumerator Helper(string sceneName) {
+        // Play animation
+        transition.SetTrigger("Start");
+
+        // Wait for animation to finish running
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(sceneName);
+    }
+}
