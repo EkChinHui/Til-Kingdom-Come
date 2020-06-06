b5242ce3b86cca166c29144fa1b3b642fa2d607e
wenhao <wenhaogoh1998@gmail.com>
2020-06-07 01:44:51 +0800

    Add sword sound effects

--- Til Kingdom Come/Assets/Scripts/GamePlay/Skills/Attack.cs
+++ Til Kingdom Come/Assets/Scripts/GamePlay/Skills/Attack.cs
@@ -38,38 +38,45 @@ namespace GamePlay.Skills
             Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attRange, playerLayer);
             // maximum distance between both players for attack to be successful
             float attackDistance = 1.934f + attRange;
-
-            // Damage enemies
-            foreach(Collider2D enemy in hitEnemies)
+            if (hitEnemies.Length == 0) {
+                AudioManager.instance.Play("Sword Swing");
+            }
+            else
             {
-                Debug.Log(enemy.name);
-                if (enemy.GetComponent<PlayerController>() == null) continue;
-                PlayerController otherPlayer = enemy.GetComponent<PlayerController>();
-                float enemyDirection = otherPlayer.transform.rotation.eulerAngles.y;
-                float myDirection = player.transform.rotation.eulerAngles.y;
-                if (otherPlayer.combatState == PlayerController.CombatState.Blocking && Math.Abs(enemyDirection - myDirection) > 1f - Mathf.Epsilon)
+                // Damage enemies
+                foreach(Collider2D enemy in hitEnemies)
                 {
-                    // other player successfully defends against attack
-                    player.KnockBack(KnockDistAttacking);
-                    otherPlayer.KnockBack(KnockDistBlocking);
-                }
-                else if (otherPlayer.combatState == PlayerController.CombatState.Rolling)
-                {
-                    // the enemy is rolling and is invulnerable
-                    return;
-                }
-                else if (otherPlayer.combatState == PlayerController.CombatState.Attacking && 
-                         Mathf.Abs(otherPlayer.transform.position.x - transform.position.x) <= attackDistance)
-                {
-                    // the enemy attacked first and is in range
-                    return;
-                }
-                else
-                {
-                    player.otherPlayer.TakeDamage(Damage);
+                    Debug.Log(enemy.name);
+                    if (enemy.GetComponent<PlayerController>() == null) continue;
+                    PlayerController otherPlayer = enemy.GetComponent<PlayerController>();
+                    float enemyDirection = otherPlayer.transform.rotation.eulerAngles.y;
+                    float myDirection = player.transform.rotation.eulerAngles.y;
+                    if (otherPlayer.combatState == PlayerController.CombatState.Blocking && Math.Abs(enemyDirection - myDirection) > 1f - Mathf.Epsilon)
+                    {
+                        // other player successfully defends against attack
+                        AudioManager.instance.Play("Swords Collide");
+                        player.KnockBack(KnockDistAttacking);
+                        otherPlayer.KnockBack(KnockDistBlocking);
+                    }
+                    else if (otherPlayer.combatState == PlayerController.CombatState.Rolling)
+                    {
+                        // the enemy is rolling and is invulnerable
+                        AudioManager.instance.Play("Sword Swing");
+                        return;
+                    }
+                    else if (otherPlayer.combatState == PlayerController.CombatState.Attacking && 
+                            Mathf.Abs(otherPlayer.transform.position.x - transform.position.x) <= attackDistance)
+                    {
+                        // the enemy attacked first and is in range
+                        return;
+                    }
+                    else
+                    {
+                        AudioManager.instance.Play("Decapitation");
+                        player.otherPlayer.TakeDamage(Damage);
+                    }
                 }
             }
-            
         }
         
         private IEnumerator AttackAnimDelay(PlayerController player)
